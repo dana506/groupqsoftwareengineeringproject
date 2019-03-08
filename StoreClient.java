7b43359b5b1d5604761c27a4ec8c132363f5c7e2
dana506 <38408523+dana506@users.noreply.github.com>
2019-03-08 10:26:54 +0000

    Add files via upload

---
+++ StoreClient.java
@@ -0,0 +1,52 @@
+package StoreMode;
+
+import java.io.*;
+
+import java.net.*;
+
+ /**
+  * 
+  * @author Dana
+  *
+  */
+
+public class StoreClient {
+	/**
+	 * Implements the client program that speaks to the StoreServer
+	 * @throws UnknownHostException Doesn't recognise the host
+	 * @throws IOException Can't get I/O for the connection to hostName
+	 * 
+	 */
+    public static void main(String[] args) throws IOException {
+         
+        if (args.length != 2) {
+            System.err.println(
+                "Usage: java Client <host name> <port number>");
+            System.exit(1);
+        }
+        
+        //Class uses the second command line argument as the port number when creating its socket
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+ 
+        //  Socket that is connected to the server running on the specified host name and port
+        try (
+            Socket storeSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(storeSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(storeSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            
+    
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        }
+    }
+}
