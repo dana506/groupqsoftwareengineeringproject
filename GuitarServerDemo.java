9e51152e151614ba340058d146478adc083d1347
dana506 <38408523+dana506@users.noreply.github.com>
2019-03-22 07:14:18 +0000

    Create GuitarServerDemo.java

---
+++ GuitarServerDemo.java
@@ -0,0 +1,32 @@
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.InetAddress;
+
+public class GuitarServerDemo {
+
+/** 
+* 
+*@author Dana Buzatu
+*Guitar Sever, here acts as a client, receiving bundles from Store Manager Mode (acting as server)
+*
+*/
+
+	private static Socket sgs; //guitar server socket end
+	private static FileOutputStream fsm;
+	InetAddress inetAddress = InetAddress.getLocalHost(); //getting the host code
+	
+	public static void main(String[] args) throws Exception {
+		
+		byte []b= new byte [20002];
+		
+		sgs = new Socket (inetAddress.getHostAddress(), 8080);
+		InputStream igs = sgs.getInputStream();
+		
+		fsm = new FileOutputStream ("C:/Users/Dana/Desktop/filemoved.zip"); //rename the file to be moved
+		
+		igs.read(b, 0, b.length);
+		fsm.write(b, 0, b.length);
+
+	}
+}
